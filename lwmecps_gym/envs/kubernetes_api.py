8f0f0a180cd74a058248506878e22d9149f92791
Ivan Filianin <adeptvin1@gmail.com>
2024-07-22 09:52:23 +0400

    Add k8s action code

--- lwmecps_gym/envs/kubernetes_api.py
+++ lwmecps_gym/envs/kubernetes_api.py
@@ -2,19 +2,34 @@
 from kubernetes import client, config
 
 class k8s:
-    def __init__(self, namespace) -> None:
+    def __init__(self, ) -> None:
         config.load_kube_config()
-        self.namespace = namespace
-        self.api = client.CoreV1Api()
+        self.core_api = client.CoreV1Api()
+        self.app_api = client.AppsV1Api()
 
         pass
 
     def k8s_state(self):
-        #   need to get per node 'cpu', 'ram', 'tx_bandwidth', 'rx_bandwidth', 'read_disks_bandwidth', 'write_disks_bandwidth'
-        list_nodes = self.api.list_node()
+        state = {}
+        #   need to get per node 'cpu', 'ram', #not possible -> 'tx_bandwidth', 'rx_bandwidth', 'read_disks_bandwidth', 'write_disks_bandwidth'
+        list_nodes = self.core_api.list_node()
         for node in list_nodes.items:
-            print(node.status.capacity)
-        pass
+            state[node.status.addresses[1].address] = node.status.capacity
+        return state
 
-    def k8s_action():
+    def k8s_action(self, namespace, deployment_name, replicas, node):
+        deployment = self.app_api.read_namespaced_deployment(
+            name=deployment_name,
+            namespace=namespace
+        )
+        deployment.spec.replicas = replicas
+        deployment.spec.template.spec.node_selector = {
+            "kubernetes.io/hostname" : node
+        }
+
+        self.app_api.patch_namespaced_deployment(
+            name=deployment_name,
+            namespace=namespace,
+            body=deployment
+        )
         pass
--- lwmecps_gym/envs/test.py
+++ lwmecps_gym/envs/test.py
@@ -2,8 +2,11 @@
 from kubernetes_api import k8s
 import time
 
-minikube = k8s(namespace='default')
+minikube = k8s()
 
-for i in range(10):
-    minikube.k8s_state()
+for i in range(1):
+    state = minikube.k8s_state()
+    print(state)
     time.sleep(1)
+
+minikube.k8s_action(namespace='default', deployment_name='time-server', replicas=3, node='minikube-m02')
