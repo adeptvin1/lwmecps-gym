f39e3e332e9642f7bd6601340dba63b455fd0eba
Ivan Filianin <adeptvin1@gmail.com>
2024-08-08 21:10:09 +0400

    Add changes to collect data from k8s

--- lwmecps_gym/envs/kubernetes_api.py
+++ lwmecps_gym/envs/kubernetes_api.py
@@ -14,8 +14,43 @@ class k8s:
         state = {}
         #   need to get per node 'cpu', 'ram', #not possible -> 'tx_bandwidth', 'rx_bandwidth', 'read_disks_bandwidth', 'write_disks_bandwidth'
         list_nodes = self.core_api.list_node()
+        all_pods = self.core_api.list_pod_for_all_namespaces()
+        all_namespaces = self.core_api.list_namespace()
+        block_ns = ['kube-node-lease', 'kube-public', 'kube-system', 'kubernetes-dashboard']
+        namespaces = []
+        for namespace in all_namespaces.items:
+            namespaces.append(namespace.metadata.name)
+        for ns in block_ns:
+            namespaces.remove(ns)
+        pod_count = {}
+        for namespace in namespaces:            
+            pod_count[namespace] = {}
+        print(namespaces)
         for node in list_nodes.items:
-            state[node.status.addresses[1].address] = node.status.capacity
+            node_name = node.status.addresses[1].address
+            state[node_name] = node.status.capacity
+            state[node_name]['deployments'] = {}
+
+
+            for pod in all_pods.items:
+                if pod.spec.node_name == node_name:
+                    
+                    for namespace in namespaces:
+                        if pod.metadata.namespace == namespace:
+                            try: 
+                                if not pod_count[namespace][pod.metadata.labels['app']]:
+                                    pass
+                            except KeyError:
+                                pod_count[namespace][pod.metadata.labels['app']] = 1
+                                pass
+                            
+                            state[node_name]['deployments'][namespace] = {
+                                    pod.metadata.labels['app']: {
+                                        'replicas': pod_count[namespace][pod.metadata.labels['app']]
+                                    }
+                            }
+
+                            pod_count[namespace][pod.metadata.labels['app']] = pod_count[namespace][pod.metadata.labels['app']] + 1
         return state
 
     def k8s_action(self, namespace, deployment_name, replicas, node):
@@ -45,6 +80,9 @@ class k8s:
                 name=deployment_name,
                 namespace=namespace
             )
+            if deployment.spec.replicas == 0:
+                print("Deployment " + deployment_name + " is scaled to 0.")
+                break
             if deployment.status.ready_replicas == deployment.spec.replicas:
                 print("Deployment " + deployment_name + " is ready.")
                 break
--- lwmecps_gym/envs/lwnecps.py
+++ lwmecps_gym/envs/lwnecps.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 from gymnasium import spaces
 from lwmecps_gym.envs.kubernetes_api import k8s
 import numpy as np
+import bitmath
+import re
 
 class LWMECPSEnv(gym.Env):
 
@@ -46,7 +48,6 @@ class LWMECPSEnv(gym.Env):
                 ) for node in self.node_name
             }
         )
-        self.observation_space['pod_node']  = spaces.Text(64)
         self.state = None
         self.reset()
 
@@ -62,57 +63,74 @@ class LWMECPSEnv(gym.Env):
                 'avg_latency': self.node_info[node]['avg_latency'],
                 'deployments': {
                     deployment: {
-                        'replicas': 1  # Пример числового значения
+                        'replicas': 0  # Пример числового значения
                     } for deployment in self.deployments
                 }
             } for node in self.node_name
-            
         }
-        self.state['pod_node'] = self.node_name[0]
-        
-        self.minikube.k8s_action(namespace=self.namespace, deployment_name=self.deployment_name, replicas=1, node=self.node_name[0])
+        for node in self.state:
+            for deployment_name in self.state[node]['deployments']:
+                self.minikube.k8s_action(namespace=self.namespace, deployment_name=self.deployment_name, replicas=1, node=node)
 
         return self.state
     
 
     def step(self, action):
         assert self.action_space.contains(action), "Invalid action"
-
-        # Текущая нода, на которой находится pod
-        current_pod_node = self.state['pod_node']
-        
+        new_state = self.k8s_state_gym()
+        print(new_state)
         # Перемещение pod на новую ноду
         pod_node = self.node_name[action]
-        self.minikube.k8s_action(namespace=self.namespace, deployment_name=self.deployment_name, replicas=1, node=self.node_name[action])
+        self.minikube.k8s_action(namespace=self.namespace, deployment_name=self.deployment_name, replicas=1, node=pod_node)
 
-        node_parameters = ['cpu', 'ram', 'tx_bandwidth', 'rx_bandwidth', 'read_disks_bandwidth', 'write_disks_bandwidth']
-        # Освобождение ресурсов на текущей ноде
-        for param in node_parameters:
-            self.state[current_pod_node][param] = min(self.state[current_pod_node][param] + self.pod_usage[param], self.node_info[current_pod_node][param])
+        # node_parameters = ['cpu', 'ram', 'tx_bandwidth', 'rx_bandwidth', 'read_disks_bandwidth', 'write_disks_bandwidth']
+        # # Освобождение ресурсов на текущей ноде
+        # for param in node_parameters:
+        #     self.state[current_pod_node][param] = min(self.state[current_pod_node][param] + self.pod_usage[param], self.node_info[current_pod_node][param])
        
         
-        # Потребление ресурсов на новой ноде
-        for param in node_parameters:
-            self.state[pod_node][param] -= self.pod_usage[param]
+        # # Потребление ресурсов на новой ноде
+        # for param in node_parameters:
+        #     self.state[pod_node][param] -= self.pod_usage[param]
 
-        # Проверка переполнения ресурсов на новой ноде
-        if self.state[pod_node]['cpu'] < 0 or self.state[pod_node]['ram'] < 0 or self.state[pod_node]['tx_bandwidth'] < 0 or self.state[pod_node]['rx_bandwidth'] < 0 or self.state[pod_node]['read_disks_bandwidth'] < 0 or self.state[pod_node]['write_disks_bandwidth'] < 0 :
-            done = True
-            reward = -100  # Негативное вознаграждение за попытку переполнения ресурсов
-        else:
-            done = False
-            reward = 0  # Можно настроить вознаграждение по-другому в зависимости от задачи
+        # # Проверка переполнения ресурсов на новой ноде
+        # if self.state[pod_node]['cpu'] < 0 or self.state[pod_node]['ram'] < 0 or self.state[pod_node]['tx_bandwidth'] < 0 or self.state[pod_node]['rx_bandwidth'] < 0 or self.state[pod_node]['read_disks_bandwidth'] < 0 or self.state[pod_node]['write_disks_bandwidth'] < 0 :
+        #     done = True
+        #     reward = -100  # Негативное вознаграждение за попытку переполнения ресурсов
+        # else:
+        #     done = False
+        #     reward = 0  # Можно настроить вознаграждение по-другому в зависимости от задачи
 
-        # Обновление pod node
-        self.state['pod_node'] = self.node_name[action]
+        # # Обновление pod node
+        # self.state['pod_node'] = self.node_name[action]
 
         info = {}
 
-        return self.state, reward, done, info
+        return self.state, # reward, done, info
+
+
+    def k8s_state_gym(self):
+        k8s_state_now = self.minikube.k8s_state()
+        self.state = {
+            node: {
+                'cpu': int(k8s_state_now[node]['cpu']),
+                'ram': round(bitmath.KiB(int(re.findall(r'\d+', k8s_state_now[node]['memory'])[0])).to_MB().value),
+                'tx_bandwidth': self.node_info[node]['tx_bandwidth'],
+                'rx_bandwidth': self.node_info[node]['rx_bandwidth'],
+                'read_disks_bandwidth': self.node_info[node]['read_disks_bandwidth'],
+                'write_disks_bandwidth': self.node_info[node]['write_disks_bandwidth'],
+                'avg_latency': self.node_info[node]['avg_latency'],
+                'deployments':  k8s_state_now[node]['deployments'][self.namespace]
+                    # deployment: {
+                    #     'replicas': 0  # Пример числового значения
+                    # } for deployment in k8s_state_now[node]['deployments'][self.namespace]
+                
+            } for node in self.node_name
+        }
+        return self.state
 
     def render(self, mode='human'):
         nodes_state = {node: {'cpu': self.state[node]['cpu'], 'ram': self.state[node]['ram'], 'avg_latency': self.state[node]['avg_latency'], 'deployments': self.state[node]['deployments']  } for node in self.node_name}
-        print(f"Nodes: {nodes_state}, Pod Node: {self.state['pod_node']}")
 
 
     def close(self):
